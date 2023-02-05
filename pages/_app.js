10f0594383dac0d553fe519c8a02b8ff3e7d7ff9
pleasantvik <adedayo.ayoola5683@gmail.com>
2023-02-05 10:37:23 +0100

    feat: created log in page and installed yup for form validation

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
 import { Header } from "@/components/Header";
 import "@/styles/globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <div>
       <Header />
       <Component {...pageProps} />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </div>
   );
 }
--- utils/schema.js
+++ utils/schema.js
@@ -1,1 +1,31 @@
-import * as yup from "yup";
+import * as Yup from "yup";
+
+export const login = Yup.object({
+  email: Yup.string()
+    .trim()
+    .required("The email field is required")
+    .email("The  email is invalid"),
+  password: Yup.string()
+    .trim()
+    .required("The password field cannot be empty")
+    .min(6, "Password should be minimum of 6 character"),
+});
+
+export const signup = Yup.object({
+  username: Yup.string()
+    .trim()
+    .required("The username field is required")
+    .min(3, "Username should be minimum of 3 character"),
+  email: Yup.string()
+    .trim()
+    .required("The email field is required")
+    .email("The  email is invalid"),
+  password: Yup.string()
+    .trim()
+    .required("The password field cannot be empty")
+    .min(6, "Password should be minimum of 6 character"),
+  confirmPassword: Yup.string()
+    .trim()
+    .oneOf([Yup.ref("password"), null], "Password does not match")
+    .required("The password field cannot be empty"),
+});
