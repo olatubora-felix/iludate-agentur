9f12c62a03780872feb829b04d81b1e5f54bcdc5
olatubora-felix <olatubora1996@gmail.com>
2023-02-07 11:24:23 +0100

    more adjustment made

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -4,19 +4,21 @@ import React from 'react'
 
 export default function signin() {
   return (
-    <MainLayout content={'A modeling and hotees site'} title="Sign In Page">
-      <main className="lg:w-[40%] w-full mx-auto pt-20 px-4 pb-4">
-        <h1 className="text-2xl text-red-900 uppercase font-serif font-bold text-center mt-6">
-          Iludate &mdash; Agentur
-        </h1>
-        <p className="text-yellow-600 font-serif uppercase text-center pt-12">
-          Log in
-        </p>
-        <p className="text-gray-500 text-sm text-center font-sans">
-          Welcome to Illudate Agentur
-        </p>
-        <Signin />
-      </main>
+    <MainLayout
+      content={'A modeling and hotees site'}
+      title="Sign In Page"
+      className="lg:w-[40%] w-full mx-auto pt-20 px-4 pb-4"
+    >
+      <h1 className="text-2xl text-red-900 uppercase font-serif font-bold text-center mt-6">
+        Iludate &mdash; Agentur
+      </h1>
+      <p className="text-yellow-600 font-serif uppercase text-center pt-12">
+        Log in
+      </p>
+      <p className="text-gray-500 text-sm text-center font-sans">
+        Welcome to Illudate Agentur
+      </p>
+      <Signin />
     </MainLayout>
   )
 }
